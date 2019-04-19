5e8fa57a00781c8a6a9a78691f0b5d8fe53a0de2
latinocodes <joselara02@outlook.com>
2019-04-19 17:45:54 -0400

    boilerplate started

--- week10/app.js
+++ week10/app.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const static = express.static(__dirname + "/public");
-
-const exphbs = require("express-handlebars");
 
 app.use("/public", static);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 app.get('*', (req, res) => {
     res.render('index', {title: 'Prime Checker', keywords: "prime checker app"})
