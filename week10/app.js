2dbfa7b084a8f9ab4b1e53bbb96033a99e7d3dc9
latinocodes <joselara02@outlook.com>
2019-04-24 13:33:06 -0400

    week10

--- week10/app.js
+++ week10/app.js
@@ -2,10 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use("/public", static);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 app.get('*', (req, res) => {
     res.render('index', {title: 'Prime Checker', keywords: "prime checker app"})
